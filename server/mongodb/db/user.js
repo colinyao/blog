7440593e97ae70b43c4b9347354192a5fb8fef6c
colinyao <yaobinhan479@C02TC4NTFVH3.local>
2017-06-22 18:17:37 +0800

    Update

--- server/mongodb/db/user.js
+++ server/mongodb/db/user.js
@@ -1,6 +1,10 @@
 const UserSchema=require('../schema/user.js')
 const db=require('../index.js')
 const UserModel=db.model('User',UserSchema)
+UserSchema.static.methods.findUser=(name)=>{
+     return this.model()
+}
+
 
 
 
