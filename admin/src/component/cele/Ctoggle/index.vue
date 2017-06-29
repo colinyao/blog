e31545a4e509af107d20a6d699eeb9d92e0007f8
colinyao <yaobinhan@yaobinhandeMacBook-Pro.local>
2017-06-30 00:10:13 +0800

    update

---
+++ admin/src/component/cele/Ctoggle/index.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="c_toggle">
+	        <div class="c_toggle_slide"><i></i></div>
+		  <input type="checkbox" class="hidden" :disabled="disabled" :checked="checked">
+	</div>
+</template>
+<script>
+	export default{
+		props:{ 
+			disabled:{
+				type:[String,Boolean]
+			},
+			checked:{
+				type:[String,Boolean],
+				default:false
+			}
+		},
+		data(){
+			return{
+                     defaultChecked:''
+			}
+		},
+		created(){
+			this.defaultChecked=this.checked
+		}
+	}
+</script>
+<style lang="less">
+.c_toggle{
+	display: inline-block;
+}
+.c_toggle_slide{
+	position: relative;
+	width:60px;
+	height:24px;
+	background:#eee;
+	border-radius: 25px;
+	i{
+		position: absolute;
+		display: block;
+		width:30px;
+		height:30px;
+		top:-3px;
+		left:-3px;
+		border-radius: 100%;
+		background:#fff;
+		box-shadow: 0 0 1px #898989;
+	}
+}
+</style>
--- admin/src/container/Login.vue
+++ admin/src/container/Login.vue
@@ -4,6 +4,7 @@
       <Cell><Cinput type="text"  v-model="name"></Cinput></Cell>
       <Cell><Cinput type="password" title="密码" :titleStyle="{paddingRight:'20px'}" leftPos="inner" rightPos="outer" v-model="password"><span slot="left" class="icon"><img src="../../static/images/icon/phone.svg"></span></Cinput></Cell>    
               <input type="button" name="" value="提交" @click='submit'>
+      <Cell><Ctoggle></Ctoggle></Cell>
       </div>
   </div>
 </template>
@@ -12,11 +13,13 @@
 import axios from 'axios'
 import Cell from '../component/cele/Cell.vue'
 import Cinput from '../component/cele/Cinput'
+import Ctoggle from '../component/cele/Ctoggle'
 export default {
   name: 'app',
     components: {
        Cinput,
-       Cell
+       Cell,
+       Ctoggle
     },
     data(){
          return{
