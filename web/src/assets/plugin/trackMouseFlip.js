6e775f17dda98768affb84e9672bfab40dbe6f1e
colinyao <yaobinhan479@C02TC4NTFVH3.local>
2017-11-28 18:34:54 +0800

    update

--- web/src/assets/plugin/trackMouseFlip.js
+++ web/src/assets/plugin/trackMouseFlip.js
@@ -9,8 +9,77 @@
    }
    var TrackMouseFlip=Class.create();
    TrackMouseFlip.prototype={
-      init(options){
+      init:function(options){
+         this.defaultOptions={
+            element:'',  //NodeList
+            rotateRange:'10'
+         }
 
+         this.options=this.combine(this.defaultOptions,options);
+
+
+         this.bindEvent();
+      },
+      bindEvent:function(){
+          var self=this,ops=this.options;
+          //鼠标进入
+
+          //鼠标移动
+
+          ops.element.forEach(function(ele){
+            (function(_ele){
+              _ele.addEventListener('mousemove',function(e){
+                 var offsetPos=self.offsetPos(e.target),  //元素离窗口偏差
+                     currentPos={x:e.clientX,y:e.clientY},  //鼠标离窗口的偏差
+                     currentSize={x:e.currentTarget.offsetWidth,y:e.currentTarget.offsetHeight},  //元素宽高度
+                     offsetDisRect={x:1-(currentPos.x-offsetPos.x)/(currentSize.x/2),y:1-(currentPos.y-offsetPos.y)/(currentSize.y/2)},//鼠标停留位置，离元素中心偏差
+                     radio=self.calculateRadio(offsetDisRect);
+
+                     _ele.style.transform="rotate3d("+offsetDisRect.x+","+offsetDisRect.y+",0,"+ops.rotateRange+"deg)";
+                     _ele.style.webkitTransform="rotate3d("+offsetDisRect.x+","+offsetDisRect.y+",0,"+ops.rotateRange+"deg)";
+
+              },true)
+            })(ele)
+          })
+
+
+          //鼠标移出
+      },
+      offsetPos(obj){
+         var x=obj.offsetLeft;
+         var y=obj.offsetTop;
+         if(obj.offsetParent){
+             var offset=this.offsetPos(obj.offsetParent)
+             x+=offset.x;
+             y+=offset.y;
+         }
+         return{x:x,y:y}
+      },
+      calculateRadio(offsetDisRect){
+         return {radioX:offsetDisRect.x*30,radioY:offsetDisRect.y*30}
+      },
+      rotating(radio){
+
+      },
+      combine(){
+
+         var objs=[].slice.apply(arguments);
+         var len=objs.length,newObj={},_newObj={};
+
+         if(len){
+            for(var i in objs[0]){
+              newObj[i]=objs[0][i]
+            }
+            objs.shift()
+            if(objs.length){
+              _newObj=this.combine.apply(this,objs)
+              console.log(newObj)
+              for(var i in _newObj){
+                newObj[i]=_newObj[i]
+              }
+            }
+         }
+         return newObj;
       }
    }
 
--- web/src/views/Test/index.js
+++ web/src/views/Test/index.js
@@ -2,12 +2,12 @@ import React,{Component} from 'react';
 import PropTypes from 'prop-types'
 import trackMouseFilp from '../../assets/plugin/trackMouseFlip'
 
-class Home extends Component{
+class Test extends Component{
        constructor(props){
             super(props)
        }
-       componentWillMount(){
-          console.log(new trackMouseFilp())
+       componentDidMount(){
+         new trackMouseFilp({element:document.querySelectorAll('.wrap')})
        }
        componentWillReceiveProps(){
 
@@ -16,12 +16,37 @@ class Home extends Component{
 
 
             return(<div className="content">
-                   test
+                    <div className="wrap" style={style.wrap}>
+                         <div style={style.content}><div style={style.test}>test</div><div style={style.test1}>test</div></div>
+                    </div>
                 </div>)
        }
 
 }
 
+let style={
+  wrap:{
+      width:'200px',
+      height:'200px',
+      margin:'100px 100px',
+      perspective:'1000px',
+      background:'#ff1100'
+  },
+  content:{
+      width:'100%',
+      height:'100%',
+  },
+  test:{
+    width:'50px',
+    height:'50px',
+    background:'#eee'
+  },
+  test1:{
+    width:'50px',
+    height:'50px',
+    background:'green'
+  }
+}
 
 
-export default Home
+export default Test
